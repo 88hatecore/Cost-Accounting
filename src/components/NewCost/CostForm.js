f42bf8a12aa00e0f330909ab488dd76190ffd43d
Ilya Dolgov <prhatecore@gmail.com>
2023-02-18 15:18:44 +0300

    ReactCourseV8useState

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,23 +1,50 @@
-import "./CostForm.css"
+import { useState } from "react";
+import "./CostForm.css";
 
 const CostForm = () => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [data, setDate] = useState("");
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const dataChangeHandler = (event) => {
+    setDate(event.taget.value);
+  };
+
   return (
     <form>
       <div className="new-cost__controls">
         <div className="new-cost__control">
           <label>Название</label>
-          <input type="text" />
+          <input type="text" onChange={nameChangeHandler} />
         </div>
         <div className="new-cost__control">
           <label>Сумма</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-cost__control">
           <label>Дата</label>
-          <input type="date" min="2019-01-01" step="2022-12-31" />
+          <input
+            type="date"
+            onChange={dataChangeHandler}
+            min="2019-01-01"
+            step="2022-12-31"
+          />
         </div>
         <div className="new-cost__actions">
-            <button type="submit">Добавить расход</button>
+          <button type="submit">Добавить расход</button>
         </div>
       </div>
     </form>
