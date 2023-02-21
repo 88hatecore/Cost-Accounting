617c722ea00e1215e41029bc29de81084ba4d96e
Ilya Dolgov <prhatecore@gmail.com>
2023-02-21 16:09:29 +0300

    ReactCourseV10

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,8 @@
-import CostItem from "./CostItem";
 import CostsFilter from "./CostsFilter";
 import "./Costs.css";
 import Card from "../UI/Card";
 import { useState } from "react";
+import CostList from "./CostList";
 
 const Costs = (props) => {
   const [selectedYear, setSelectedYear] = useState("2023");
@@ -10,25 +10,15 @@ const Costs = (props) => {
     setSelectedYear(year);
   };
 
+  const filteredCosts = props.costs.filter((cost) => {
+    return cost.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        <CostItem
-          date={props.costs[0].date}
-          description={props.costs[0].description}
-          amount={props.costs[0].amount}
-        />
-        <CostItem
-          date={props.costs[1].date}
-          description={props.costs[1].description}
-          amount={props.costs[1].amount}
-        />
-        <CostItem
-          date={props.costs[2].date}
-          description={props.costs[2].description}
-          amount={props.costs[2].amount}
-        />
+        <CostList costs={filteredCosts}/>
       </Card>
     </div>
   );
